a948ae1c0e195ed9a422fad94169833908db8d37
codeamin11 <legacyofalamin@gmail.com>
2024-06-03 00:14:57 +0600

    firebase set up

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Toaster } from 'react-hot-toast'
+import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 
 import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './routes/Routes.jsx';
+import AuthProvider from '../providers/AuthProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+    <AuthProvider>
+      <RouterProvider router={router} />
+      <Toaster />
+    </AuthProvider>
+    </HelmetProvider>
   </React.StrictMode>,
 )
