b39ccb1fddcc2b9cf99401464907e213bf6f2959
codeamin11 <legacyofalamin@gmail.com>
2024-06-03 21:56:09 +0600

    biodata page filter form added

--- src/pages/Biodatas/Biodatas.jsx
+++ src/pages/Biodatas/Biodatas.jsx
@@ -1,7 +1,8 @@
 import {useQuery} from '@tanstack/react-query'
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import LoadingSpinner from '../../components/Shared/LoadingSpinner';
-import BiodataDetails from '../BiodataDetails/BiodataDetails';
+import BiodataDetails from './BiodataDetails';
+import BiodataFilterForm from './BiodataFilterForm';
 const Biodatas = () => {
     const axiosSecure = useAxiosSecure()
 
@@ -16,12 +17,12 @@ const Biodatas = () => {
     if(isLoading) return <LoadingSpinner />
     return (
         <div className="flex flex-col md:flex-row md:p-4">
-            <div className="md:w-4/12">
-           
+            <div className="md:w-4/12 lg:w-3/12">
+                <BiodataFilterForm></BiodataFilterForm>
             </div>
 
             {/* all biodatas div */}
-            <div className="flex-1 md:ml-8">
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 flex-1 md:ml-8 p-2">
                 {
                     biodatas.map(biodata => <BiodataDetails key={biodata._id} biodata={biodata}></BiodataDetails>)
                 }
