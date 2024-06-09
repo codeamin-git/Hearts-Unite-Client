9d68485c13c6805e9ec7fda5b914418a25418054
codeamin11 <legacyofalamin@gmail.com>
2024-06-10 00:50:29 +0600

    admin's approved premium page set functional

--- src/pages/Dashboard/Admin/ApprovedPremium.jsx
+++ src/pages/Dashboard/Admin/ApprovedPremium.jsx
@@ -1,9 +1,69 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import LoadingSpinner from "../../../components/Shared/LoadingSpinner";
+import toast from "react-hot-toast";
+import { Button, Table } from "flowbite-react";
+import { GiCheckMark } from "react-icons/gi";
 
 const ApprovedPremium = () => {
+    const axiosSecure = useAxiosSecure()
+    const {data: biodatas = [], refetch, isLoading} = useQuery({
+        queryKey: ['makePremium'],
+        queryFn: async () => {
+            const {data} = await axiosSecure.get('/biodatas')
+            const premiumReq = data.filter((req)=> req.biodataStatus === 'Requested')
+            console.log(premiumReq);
+            return premiumReq;
+        }
+    })
+
+    const handleApprove = async id =>{
+        try{
+            const {data} = await axiosSecure.patch(`/makePremium/${id}`)
+            console.log(data);
+            if(data.modifiedCount > 0){
+                toast.success('Biodata made premium successfully!')
+                refetch()
+            }
+        }catch(err){
+            console.log(err);
+        }
+        console.log(id);
+    }
+    if(isLoading) return <LoadingSpinner />
+
     return (
-        <div>
-            <h2 className="text-3xl">Approved premium route</h2>
-        </div>
+        <div className="overflow-x-auto">
+      <Table hoverable>
+        <Table.Head>
+          <Table.HeadCell>Name</Table.HeadCell>
+          <Table.HeadCell>Email</Table.HeadCell>
+          <Table.HeadCell>Biodata Id</Table.HeadCell>
+          <Table.HeadCell>Make Premium</Table.HeadCell>
+        </Table.Head>
+        <Table.Body className="divide-y">
+          {
+            biodatas?.map(biodata => <Table.Row key={biodata._id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                {biodata?.name}
+                </Table.Cell>
+                <Table.Cell>{biodata?.contactEmail}</Table.Cell>
+                <Table.Cell>
+                    {biodata?.biodataId}
+                </Table.Cell>
+                <Table.Cell>
+                <Button gradientDuoTone="purpleToPink" className="p-0 flex items-center" onClick={() =>handleApprove(biodata?._id)
+                    }
+                    ><GiCheckMark className="text-xl mr-2"/> Approve</Button>
+                </Table.Cell>
+              </Table.Row>
+            )
+          }
+        </Table.Body>
+      </Table>
+
+      
+    </div>
     );
 };
 
